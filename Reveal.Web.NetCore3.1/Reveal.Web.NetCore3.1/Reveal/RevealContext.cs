659e702389067a8152bec78c73146cb10a4784a4
Konstantin Koynov <kkoynov@infragistics.com>
2020-12-10 11:53:19 +0200

    Added a dummy implementation RevealSdkContextBase class called RevealContext. Modified the Startup's ConfigureServices so it configure Reveal data cache locations. Also setting up the location of the local datasouce files to be LocalDatasourceFiles folder inside of the wwwroot. Added Samples.xlsx file which we'll need later when trying to load one of the samples dashboards.

---
+++ Reveal.Web.NetCore3.1/Reveal.Web.NetCore3.1/Reveal/RevealContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Reveal.Sdk;
+
+namespace Reveal.Web.NetCore3._1.Reveal
+{
+    public class RevealContext : RevealSdkContextBase
+    {
+        public override Task<Dashboard> GetDashboardAsync(string dashboardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task SaveDashboardAsync(string userId, string dashboardId, Dashboard dashboard)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Reveal.Web.NetCore3.1/Reveal.Web.NetCore3.1/Startup.cs
+++ Reveal.Web.NetCore3.1/Reveal.Web.NetCore3.1/Startup.cs
@@ -4,8 +4,11 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Reveal.Sdk;
+using Reveal.Web.NetCore3._1.Reveal;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -27,6 +30,24 @@ namespace Reveal.Web.NetCore3._1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews().AddNewtonsoftJson();
+
+            var embedSettings = new RevealEmbedSettings();
+            embedSettings.LocalFileStoragePath = GetLocalFileStoragePath(_webRootPath);
+
+            //You could configure the default disk locations used by RevealView to store cached data by uncommenting the following lines:
+            var cacheFilePath = Configuration.GetSection("Caching")?["CacheFilePath"] ?? @"C:\Temp\Reveal\Web\Cache";
+            Directory.CreateDirectory(cacheFilePath);
+            embedSettings.DataCachePath = cacheFilePath;
+            embedSettings.CachePath = cacheFilePath;
+
+            services.AddRevealServices(embedSettings, new RevealContext());
+
+            services.AddMvc().AddReveal().AddNewtonsoftJson();
+        }
+
+        protected virtual string GetLocalFileStoragePath(string webRootPath)
+        {
+            return Path.Combine(webRootPath, "LocalDatasourceFiles");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
