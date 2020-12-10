704553c2a3b1d5e204e2e8e2520bbd384b8bc891
Konstantin Koynov <kkoynov@infragistics.com>
2020-12-10 14:47:35 +0200

    Added Roboto fonts in wwwroot/fonts/ which Reveal Sdk needs. Added font-face entries in the site.css. Added a small class in site.js so we could make sure Roboto fonts are loaded prior to rendering a dashboard. Modified the index.cshtml. Added a reference to webfont loader in the  _Layout.cshtml.

--- Reveal.Web.NetCore3.1/Reveal.Web.NetCore3.1/wwwroot/js/site.js
+++ Reveal.Web.NetCore3.1/Reveal.Web.NetCore3.1/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+class RobotoFontsLoader {
+    _fontsLoaded = false;
+    Load() {
+        var loader = this;
+        return new Promise((successHandler, errorHandler) => {
+            if (loader._fontsLoaded) {
+                successHandler();
+            }
+            try {
+                WebFont.load({
+                    custom: {
+                        families: ['Roboto-Regular', 'Roboto-Bold', 'Roboto-Medium', 'Roboto-Light']
+                    },
+                    active: function () {
+                        loader._fontsLoaded = true;
+                        successHandler();
+                    },
+                    fontinactive: function(familyName, fvd) {
+                        errorHandler(familyName + " failed to load")
+                    }
+                })
+            } catch (error) {
+                errorHandler(error)
+
+            }
+        });
+    }
+}
+
+window.RobotoFontsLoader = new RobotoFontsLoader();
+window.RobotoFontsLoader.Load();
